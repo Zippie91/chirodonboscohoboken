ab1be93f57e99ffff62d63899806a57648e0c8fa
Raf Smits <raf_smits@hotmail.com>
2016-02-07 21:13:47 +0100

    Add Bootstrap & Change libraries to assets

--- wwwroot/index.php
+++ wwwroot/index.php
@@ -1,9 +1,25 @@
 <!DOCTYPE html>
 <html>
 <head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
   <title>Chiro Don Bosco Hoboken</title>
+
+  <!-- Bootstrap -->
+  <link href="/assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Optional Bootstrap Theme -->
+  <link href="/assets/bootstrap/css/bootstrap-theme.min.css" rel="stylesheet">
+
 </head>
 <body>
   <h1>Chiro Don Bosco</h1>
+
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <!-- Include all compiled plugins (below), or include individual files as needed -->
+  <script src="/assets/bootstrap/js/bootstrap.min.js"></script>
 </body>
 </html>
