6dc2ffb8483d103aca274c82d0ef9218e671721c
Raf Smits <raf_smits@hotmail.com>
2016-02-10 21:16:45 +0100

    Add navbar

--- wwwroot/index.php
+++ wwwroot/index.php
@@ -24,6 +24,53 @@
 
 </head>
 <body>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-1" aria-expanded="false">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">Brand</a>
+      </div>
+
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="navbar-collapse-1">
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Afdelingen <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a href="#">Speelclub</a></li>
+              <li><a href="#">Rakwi's</a></li>
+              <li><a href="#">Tito's</a></li>
+              <li><a href="#">Aspi's</a></li>
+            </ul>
+          </li>
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Werkgroepen <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a href="#">Leiding</a></li>
+              <li><a href="#">VeeBee</a></li>
+              <li><a href="#">Ol&eacute;</a></li>
+              <li><a href="#">VZW</a></li>
+            </ul>
+          </li>
+          <li><a href="#">Verhuur</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+        <form class="navbar-form navbar-right" role="search">
+          <div class="form-group">
+            <input type="text" class="form-control" placeholder="Search">
+          </div>
+          <button type="submit" class="btn btn-default">Zoek</button>
+        </form>
+      </div>
+    </div>
+  </nav>
   <h1>Chiro Don Bosco</h1>
 
   <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
