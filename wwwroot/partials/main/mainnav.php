fba4665b4a031ddd7c1fe74808ea66c15cc9eb83
Raf Smits <raf_smits@hotmail.com>
2016-09-26 19:51:44 +0200

    Make partials, create partials folder, change index.php to use partials

---
+++ wwwroot/partials/main/mainnav.php
@@ -0,0 +1,49 @@
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">
+        <img alt="Brand" src="/images/logodbh.png" />
+      </a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Afdelingen <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Speelclub</a></li>
+            <li><a href="#">Rakwi's</a></li>
+            <li><a href="#">Tito's</a></li>
+            <li><a href="#">Aspi's</a></li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Werkgroepen <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Leiding</a></li>
+            <li><a href="#">VeeBee</a></li>
+            <li><a href="#">Ol&eacute;</a></li>
+            <li><a href="#">VZW</a></li>
+          </ul>
+        </li>
+        <li><a href="#">Verhuur</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+      <form class="navbar-form navbar-right" role="search">
+        <button type="button" class="btn btn-default">
+          <span class="glyphicon glyphicon-user"></span>
+          <span class="sr-only">Login</span>
+        </button>
+      </form>
+    </div>
+  </div>
+</nav>
---
+++ wwwroot/partials/main/sidebar.php
@@ -0,0 +1,136 @@
+<!-- Sidebar Offcanvas -->
+<div class="col-xs-6 col-sm-3 sidebar-offcanvas" id="sidebar">
+  <div class="list-group">
+    <!-- Sidebar Alerts -->
+    <a class="list-group-item" href="#collapseAlert" role="button" data-toggle="collapse" aria-expanded="false" aria-controls="collapseAlert">
+      Alerts
+      <span class="badge">42</span>
+    </a>
+    <div id="collapseAlert" class="collapse">
+      <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+        <div class="panel panel-default">
+          <div class="panel-heading" role="tab" id="headingOne">
+            <h4 class="panel-title">
+              <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                Speelclub
+              </a>
+            </h4>
+          </div>
+          <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+            <div class="panel-body">
+              Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+            </div>
+          </div>
+        </div>
+        <div class="panel panel-default">
+          <div class="panel-heading" role="tab" id="headingTwo">
+            <h4 class="panel-title">
+              <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                Rakwi's
+              </a>
+            </h4>
+          </div>
+          <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+            <div class="panel-body">
+              Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+            </div>
+          </div>
+        </div>
+        <div class="panel panel-default">
+          <div class="panel-heading" role="tab" id="headingThree">
+            <h4 class="panel-title">
+              <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                Tito's
+              </a>
+            </h4>
+          </div>
+          <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+            <div class="panel-body">
+              Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+            </div>
+          </div>
+        </div>
+        <div class="panel panel-default">
+          <div class="panel-heading" role="tab" id="headingFour">
+            <h4 class="panel-title">
+              <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                Aspis
+              </a>
+            </h4>
+          </div>
+          <div id="collapseFour" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFour">
+            <div class="panel-body">
+              Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Sidebar Gastenboek -->
+    <a class="list-group-item" href="#collapseGast" role="button" data-toggle="collapse" aria-expanded="false" aria-controls="collapseGast">
+      Gastenboek
+    </a>
+    <div id="collapseGast" class="collapse in">
+      <div class="panel-body" id="chatbox">
+        <ul class="chat">
+          <li class="clearfix chatmsg">
+            <div class="chat-body clearfix">
+              <div class="header">
+                <strong class="primary-font">Jack Sparrow Strongarm</strong>
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet,
+              </p>
+            </div>
+          </li>
+          <li class="chatmsg clearfix">
+            <div class="chat-body clearfix">
+              <div class="header">
+                <strong class="primary-font">Bhaumik Patel</strong>
+              </div>
+              <p>
+                Er werd toen veel gezegd. Moeder stond op en keek naar Jan. Haar gezicht was vuurrood en ze maakte zich klaar om te roepen.
+              </p>
+            </div>
+          </li>
+          <li class="clearfix chatmsg">
+            <div class="chat-body clearfix">
+              <div class="header">
+                <strong class="primary-font">Jack Sparrow Strongarm</strong>
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet,
+              </p>
+            </div>
+          </li>
+          <li class="chatmsg clearfix">
+            <div class="chat-body clearfix">
+              <div class="header">
+                <strong class="primary-font">Bhaumik Patel</strong>
+              </div>
+              <p>
+                Er werd toen veel gezegd. Moeder stond op en keek naar Jan. Haar gezicht was vuurrood en ze maakte zich klaar om te roepen.
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div class="panel-footer">
+        <div class="input-group">
+          <input id="btn-input" type="text" class="form-control input-sm" placeholder="Type your message here..." />
+          <span class="input-group-btn">
+            <button class="btn btn-warning btn-sm" id="btn-chat">Stuur</button>
+          </span>
+        </div>
+      </div>
+    </div>
+    <!-- Sidebar Foto's -->
+    <a class="list-group-item" href="#collapseFoto" role="button" data-toggle="collapse" aria-expanded="false" aria-controls="collapseFoto">
+      Foto's
+    </a>
+    <div id="collapseFoto" class="collapse in">
+      <img class="img-responsive" src="/images/foto/Testbeeld1080p.png"/>
+      <!--<img class="img-responsive" src="/images/foto/testbeeld1946.jpg"/>-->
+    </div>
+  </div>
+</div>
